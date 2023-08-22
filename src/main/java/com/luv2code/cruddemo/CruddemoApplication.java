fa8dde69e55276d51b4e4a8c8bfdaa0c6c1ba0ae
Marrero, Maykel <141251534+Macko40@users.noreply.github.com>
2023-08-22 15:45:09 +0200

    Modified properties and create method createClient was modifie the spring.jpa.hiberante.ddl-auto to =none before was with =create

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -29,7 +29,7 @@ public class CruddemoApplication {
 			//queryForClient(clientDAO);
 
 			//queryForClientByLastName(clientDAO);
-			
+
 			//updateClientFirstName(clientDAO);
 
 			//deleteClient(clientDAO);
